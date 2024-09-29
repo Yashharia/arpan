dc3359f6193a62df4461947523d91016ce1b66b0
YashHaria <yash.rh@somaiya.edu>
2024-09-29 18:28:16 +0530

    reponsive

--- assets/js/general.js
+++ assets/js/general.js
@@ -431,12 +431,10 @@ jQuery(document).ready(function () {
   //faq accordion
 
   $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
-    disableOn: 700,
     type: "iframe",
     mainClass: "mfp-fade",
     removalDelay: 160,
     preloader: false,
-
     fixedContentPos: false,
   });
 }); /* ready end */
